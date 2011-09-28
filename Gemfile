5ee36bca5cce174bbd8a9617bd4e05b5eb794532
Tetsuharu Kohkaki <tetsupa0427@gmail.com>
2011-09-28 22:08:34 +0900

    first commit

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.0'
+gem 'json'
+gem 'mysql2', '~> 0.3'
+
+group :development, :test do
+  gem 'rspec-rails', '~>2.6'
+#  gem 'rr', '~> 1.0'
+#  gem 'capybara', '~> 0.4'
+#  gem 'spork',  '~> 0.9.0.rc'
+#  gem 'ruby_gntp', '~> 0.3'
+#  gem 'ir_b'
+#  gem 'watchr'
+#  gem 'capybara-mechanize'
+#  gem 'prefetch-rspec', '~> 0.1.4'
+#  gem 'database_cleaner'
+#  gem 'factory_girl_rails'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'cd77115bcab812f2a21466066bb002f82c1399f13eb6104e56785c01aaf81d2ee958ab0264f906a38bf94a3c6282a3bfb7ba9d143e6ea6875201f0e881d51024'
